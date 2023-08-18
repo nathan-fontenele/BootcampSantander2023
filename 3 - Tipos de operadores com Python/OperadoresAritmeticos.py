f2d6d5a8c31b3a747f5becd8f5ac7fb6e77d4843
Nathan Fontenele <nathangomes.st@gmail.com>
2023-08-18 16:30:18 -0300

    Aula sobre operadores em Pyhton

---
+++ 3 - Tipos de operadores com Python/OperadoresAritmeticos.py
@@ -0,0 +1,22 @@
+soma = 2 + 3
+divisao = 10 / 3
+divisao_inteira = 10 // 3
+multiplicacao = 2 * 5
+resto = 10 % 3
+pontenciacao = 10 ** 2
+
+print(f"Soma {soma}")
+print(f"Divisao {divisao}")
+print(f"Divisão inteira {divisao_inteira}")
+print(f"Multiplicação {multiplicacao}")
+print(f"Resto {resto}")
+print(f"Pontênciação {pontenciacao}")
+
+'''Ordem de precedencia
+
+1° - parenteses
+2° - Expoentes
+3° - Multiplicações e divisões
+4° - Somas e subtrações
+
+'''
---
+++ 3 - Tipos de operadores com Python/OperadoresComparacao.py
@@ -0,0 +1,9 @@
+produto_1 = 54
+produto_2 = 454
+
+print(produto_1 == produto_2)
+print(produto_1 != produto_2)
+print(produto_1 > produto_2)
+print(produto_1 >= produto_2)
+print(produto_1 < produto_2)
+print(produto_1 <= produto_2)
