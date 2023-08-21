8df1fcfc536ae61c3c1444c7165663575b73fc08
Nathan Fontenele <nathangf60@outlook.com>
2023-08-20 22:23:41 -0300

    Aula de operadores lógicos

---
+++ 3 - Tipos de operadores com Python/OperadoresLogicos.py
@@ -0,0 +1,21 @@
+'''And, Or, Not'''
+'''&&, ||, !'''
+
+value1, value2, value3 = True, True, True
+
+if(value1 and value2 == value3):
+    print(True)
+else:
+    print(False)
+
+if(value1 or value2 == value3):
+    print(True)
+else:
+    print(False)
+
+if(not(value1) == value3):
+    print(True)
+else:
+    print(False)
+
+
